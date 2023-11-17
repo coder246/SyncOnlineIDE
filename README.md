e11ba45c363dff7e313224195fc80060e81a4268
Tim Arnold <46609883+Coder246@users.noreply.github.com>
2023-11-17 19:47:55 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# Online IDE Sync Script
+
+This script automates the synchronization of files from a local directory to the Online-IDE.de project. It utilizes the Online-IDE.de API to log in, clear the existing workspace, and upload Java files from the specified local directory.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Configuration](#configuration)
+- [License](#license)
+- [Author](#author)
+
+## Introduction
+
+The Online IDE Sync Script is designed to streamline the process of synchronizing your local Java files with the Online-IDE.de project. It performs login, clears the existing workspace, and uploads the Java files from the specified local directory.
+
+## Installation
+
+1. Clone the repository or download the script file.
+2. Make sure you have Node.js installed.
+3. Install dependencies by running: `npm install` or `yarn install`
+
+## Usage
+
+Download the latest release and run the .exe-file in your local directory containing the Java files you want to upload to Online-IDE.de.
+
+Don't forget to configure the credentials before running it. See [Configuration](#configuration) for more information.
+
+or
+
+Run the script using the command:
+
+```bash
+node script.js
+```
+
+The script will prompt you with updates as it progresses through the synchronization process.
+
+## Configuration
+
+Before running the script, ensure that you have a `syncconfig.json` file with the required configuration parameters:
+
+- `user`: Your Online-IDE.de username.
+- `password`: Your Online-IDE.de password.
+- `workspace`: The name of the workspace on Online-IDE.de.
+- `path`: The path within the Online-IDE.de workspace. Keep empty if you want to upload to the root directory.
+- `localPath`: The local directory path for Java files. If not specified, the default is set to `./`.
+
+Example `syncconfig.json`:
+
+```json
+{
+  "user": "your_username",
+  "password": "your_password",
+  "workspace": "your_workspace",
+  "path": "your_path",
+  "localPath": "./your_local_directory"
+}
+
+```
+
+## License
+
+This script is licensed under the [MIT License](LICENSE).
+
+## Disclaimer
+
+```
+This script is not affiliated with the Online-IDE.de project or Martin Pabst.
+```
+
---
+++ index.js
@@ -0,0 +1,211 @@
+//load syncconfig.json file
+
+import fs from "fs";
+
+async function main() {
+
+    console.log("\n" +
+        "  /$$$$$$                                       /$$$$$$            /$$ /$$                           /$$$$$$ /$$$$$$$  /$$$$$$$$\n" +
+        " /$$__  $$                                     /$$__  $$          | $$|__/                          |_  $$_/| $$__  $$| $$_____/\n" +
+        "| $$  \\__/ /$$   /$$ /$$$$$$$   /$$$$$$$      | $$  \\ $$ /$$$$$$$ | $$ /$$ /$$$$$$$   /$$$$$$         | $$  | $$  \\ $$| $$      \n" +
+        "|  $$$$$$ | $$  | $$| $$__  $$ /$$_____/      | $$  | $$| $$__  $$| $$| $$| $$__  $$ /$$__  $$ /$$$$$$| $$  | $$  | $$| $$$$$   \n" +
+        " \\____  $$| $$  | $$| $$  \\ $$| $$            | $$  | $$| $$  \\ $$| $$| $$| $$  \\ $$| $$$$$$$$|______/| $$  | $$  | $$| $$__/   \n" +
+        " /$$  \\ $$| $$  | $$| $$  | $$| $$            | $$  | $$| $$  | $$| $$| $$| $$  | $$| $$_____/        | $$  | $$  | $$| $$      \n" +
+        "|  $$$$$$/|  $$$$$$$| $$  | $$|  $$$$$$$      |  $$$$$$/| $$  | $$| $$| $$| $$  | $$|  $$$$$$$       /$$$$$$| $$$$$$$/| $$$$$$$$\n" +
+        " \\______/  \\____  $$|__/  |__/ \\_______/       \\______/ |__/  |__/|__/|__/|__/  |__/ \\_______/      |______/|_______/ |________/\n" +
+        "           /$$  | $$                                                                                                            \n" +
+        "          |  $$$$$$/                                                                                                            \n" +
+        "           \\______/                                                                                                             \n");
+
+    console.error = function() {};
+    //if file does not exist, exit
+    if (!fs.existsSync('./syncconfig.json')) {
+        console.warn("syncconfig.json not found");
+        process.exit(1);
+    }
+
+    let configJSON = JSON.parse(fs.readFileSync('./syncconfig.json', 'utf8'));
+
+    let user = configJSON.user;
+    if (user == null) {
+        console.warn("Attribute user not found in syncconfig.json");
+        process.exit(1);
+    }
+
+    let password = configJSON.password;
+    if (password == null) {
+        console.warn("Attribute password not found in syncconfig.json");
+        process.exit(1);
+    }
+
+    let workspace = configJSON.workspace;
+    if (workspace == null) {
+        console.warn("Attribute workspace not found in syncconfig.json");
+        process.exit(1);
+    }
+
+    let path = configJSON.path;
+    if (path == null) {
+        console.warn("Attribute path not found in syncconfig.json");
+        process.exit(1);
+    }
+
+    if(configJSON.localPath == null){
+        console.log("Attribute localPath not found in syncconfig.json - using default value ./");
+    }
+
+    let localPath = configJSON.localPath??"./";
+    if(localPath===""){
+        localPath = "./";
+    }
+
+    //check if localPath exists
+    if (!fs.existsSync(localPath)) {
+        console.warn("localPath does not exist");
+        process.exit(1);
+    }
+
+
+    process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+
+    let {workspaces, cookie, userID, token} = await getProjects();
+
+    //find given workspace
+
+    let syncingWorkspace = workspaces.find((workspaceObject) => {
+        return workspaceObject.name === workspace && workspaceObject.path === path && workspaceObject.isFolder === false;
+    });
+
+    if (syncingWorkspace == null) {
+        console.warn("Workspace on remote not found");
+        process.exit(1);
+    }
+
+    syncingWorkspace.files.forEach(async (file) => {
+        await deleteFile(file.id, userID, cookie, token);
+    });
+
+    console.log("Workspace cleared");
+    console.log("");
+
+    //find all files in current directory *.java
+    let localFiles = fs.readdirSync(localPath).filter((file) => {
+        return file.endsWith(".java");
+    });
+
+    //log all files
+    console.log("Files to upload:");
+    localFiles.forEach((file) => {
+        console.warn("- "+file);
+    });
+
+    //upload all files
+
+    for (let i = 0; i < localFiles.length; i++) {
+        let file = localFiles[i];
+        console.log("");
+        console.log("Uploading "+file+" ("+(i+1)+"/"+localFiles.length+")");
+        await uploadFile(file.toString(), userID, fs.readFileSync(file, 'utf8').toString(), syncingWorkspace.id, token, cookie);
+    }
+
+
+    console.log("");
+    console.log("Sync complete");
+    console.log("");
+    console.log("-------");
+    console.log("Script by Tim Arnold");
+    console.log("This script is not affiliated with the Online-IDE.de project or Martin Pabst.");
+    console.log("-------");
+
+    async function getProjects() {
+
+        let loginResult = await fetch("https://online-ide.de/servlet/login", {
+            "headers": {
+                "accept": "*/*",
+                "accept-language": "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7",
+                "cache-control": "no-cache",
+                "content-type": "application/json",
+                "pragma": "no-cache",
+                "Referer": "https://online-ide.de/",
+            },
+            "body": JSON.stringify({username: user, password: password}),
+            "method": "POST"
+        })
+
+
+        let userObject = await loginResult.json();
+        if(userObject.success === false){
+            console.warn("Login failed. Check your credentials in syncconfig.json");
+            process.exit(1);
+        }else{
+            console.log("Login successful");
+        }
+
+        let cookie = loginResult.headers.get('set-cookie').split(";")[0];
+
+        return {
+            workspaces: userObject.workspaces.workspaces,
+            cookie: cookie,
+            userID: userObject.user.id,
+            token: userObject.csrfToken
+        };
+    }
+
+    async function deleteFile(file, user, cookie, token) {
+        let result = await fetch("https://online-ide.de/servlet/createOrDeleteFileOrWorkspace", {
+            "headers": {
+                "accept": "*/*",
+                "accept-language": "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7",
+                "cache-control": "no-cache",
+                "content-type": "application/json",
+                "pragma": "no-cache",
+                "cookie": cookie,
+                "Referer": "https://online-ide.de/",
+                "Referrer-Policy": "strict-origin-when-cross-origin",
+                "x-token-pm": token,
+
+            },
+            "body": JSON.stringify({type: "delete", entity: "file", id: file, userId: user}),
+            "method": "POST"
+        });
+    }
+
+    async function uploadFile(name, user, content, workspaceID, token, cookie) {
+        let result = await fetch("https://online-ide.de/servlet/createOrDeleteFileOrWorkspace", {
+            "headers": {
+                "accept": "*/*",
+                "accept-language": "de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7",
+                "cache-control": "no-cache",
+                "content-type": "application/json",
+                "pragma": "no-cache",
+
+                "x-token-pm": token,
+                "cookie": cookie,
+                "Referer": "https://online-ide.de/",
+                "Referrer-Policy": "strict-origin-when-cross-origin"
+            },
+
+            "body": JSON.stringify({
+                type: "create",
+                entity: "file",
+                data: {
+                    name: name,
+                    text: content,
+                    text_before_revision: null,
+                    submitted_date: null,
+                    student_edited_after_revision: false,
+                    version: 1,
+                    identical_to_repository_version: false,
+                    workspace_id: workspaceID,
+                    forceUpdate: false
+                },
+                owner_id: user,
+                userId: user
+            }),
+            "method": "POST"
+        });
+
+    }
+}
+
+main();
